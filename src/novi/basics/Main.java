8cbe05030884555ce67b01212c2d3a3abf346569
Yousra Khayati <y.khayati@novi-eduction.nl>
2020-06-02 16:16:33 +0200

    logincontroller

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Hello Yousra");
+
+        LoginController loginController = new LoginController();
     }
 }
